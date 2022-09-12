b36e90c1950616b29e37168ab87923f571ef90d8
Cem PEHLİVAN <cempehlivan92@gmail.com>
2022-09-12 19:08:24 +0300

    github pages base url fix

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: process.env.NODE_ENV === "development" ? "/" : "/dist/",
+  base: process.env.NODE_ENV === "development" ? "/" : "/vue-weather-app/dist/",
 });
